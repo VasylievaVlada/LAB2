1349e0630376c89461221e72e0be4d4f9d367b4d
VasylievaVlada <vvy200230@gmail.com>
2022-02-13 18:57:25 +0300

    Добавьте файлы проекта.

---
+++ LAB2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LAB2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Address someAddress = new Address();
+
+            someAddress.Index = 12345;
+            someAddress.Country = "Ukraine";
+            someAddress.City = "Kyiv";
+            someAddress.Street = "Bulgakova";
+            someAddress.House = 20;
+            someAddress.Apartment = 356;
+
+
+            Console.WriteLine($"Index:{someAddress.Index}; Country:{someAddress.Country}; City:{someAddress.City}; Street :{someAddress.Street}; House:{someAddress.House}; Apartment:{someAddress.Index}");
+
+        
+        }
+    }
+}
